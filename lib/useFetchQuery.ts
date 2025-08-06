76c1e69a035e05e1f0a1e71f2a286e8a612656b5
Aloys CHERRIER <aloyscherry@gmail.com>
2025-08-06 13:35:23 +0200

    API : fix jwt token not send on guest routes /!\ need to be declared, cause not taken into account yet by openapi-typescript

--- lib/useFetchQuery.ts
+++ lib/useFetchQuery.ts
@@ -8,24 +8,34 @@ type PathMethod<T extends Path> = keyof paths[T];
 
 type RequestParams<
   P extends Path,
-  M extends PathMethod<P>
-> = paths[P][M] extends { parameters: any } ? paths[P][M]["parameters"] : undefined;
+  M extends PathMethod<P>,
+> = paths[P][M] extends { parameters: any }
+  ? paths[P][M]["parameters"]
+  : undefined;
 
 type RequestBody<
   P extends Path,
-  M extends PathMethod<P>
-> = paths[P][M] extends { requestBody: { content: { "application/json": infer R } } }
+  M extends PathMethod<P>,
+> = paths[P][M] extends {
+  requestBody: { content: { "application/json": infer R } };
+}
   ? R
   : undefined;
 
 type ResponseType<
   P extends Path,
-  M extends PathMethod<P>
-> = paths[P][M] extends { responses: { 200: { content: { "application/json": infer R } } } }
+  M extends PathMethod<P>,
+> = paths[P][M] extends {
+  responses: { 200: { content: { "application/json": infer R } } };
+}
   ? R
   : undefined;
 
 const endpoint = "http://" + devEnvConfig["ip"];
+
+// Since openapi-typescript doesnt support yet security schemes as for august 6 2025 (see : https://github.com/openapi-ts/openapi-typescript/issues/922)
+// We need to declare guest routes that do not require authentication
+const guestRoutes: Path[] = ["/login"];
 
 export const useFetchQuery = <P extends Path, M extends PathMethod<P>>(
   url: P,
@@ -36,29 +46,32 @@ export const useFetchQuery = <P extends Path, M extends PathMethod<P>>(
 ) => {
   const httpMethod = String(method).toUpperCase();
 
-  const queryString = httpMethod === "GET" && params
-    ? "?" + new URLSearchParams(params as Record<string, string>).toString()
-    : "";
+  const queryString =
+    httpMethod === "GET" && params
+      ? "?" + new URLSearchParams(params as Record<string, string>).toString()
+      : "";
 
   const full_url = endpoint + url + queryString;
 
   const fetchData = async (): Promise<ResponseType<P, M>> => {
-    // Wait for the JWT token to be resolved before proceeding
-    const jwt = await SecureStorage.get("userSession").then(
-      (userSession) => userSession?.jwt
-    );
+    let headers: HeadersInit = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    if (!guestRoutes.includes(url)) {
+      // Wait for the JWT token to be resolved before proceeding
+      const jwt = await SecureStorage.get("userSession").then(
+        (userSession) => userSession?.jwt
+      );
 
-    if (!jwt) {
-      throw new Error("JWT token is missing");
+      if (!jwt) {
+        throw new Error("JWT token is missing");
+      }
+      headers = { ...headers, Authorization: "Bearer " + jwt };
     }
-
     const response = await fetch(full_url, {
       method: httpMethod,
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": "Bearer " + jwt, // Use the resolved JWT here
-      },
+      headers: headers,
       body: httpMethod !== "GET" && body ? JSON.stringify(body) : undefined,
     });
 
