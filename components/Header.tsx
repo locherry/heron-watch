eef4dd18b2b591f2d666c285fbec900583c781ba
Aloys CHERRIER <aloyscherry@gmail.com>
2025-08-14 16:19:59 +0200

    UX : Added tootip for back button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import { router } from "expo-router";
+import { t } from "i18next";
 import { ViewProps } from "react-native";
 import { ChevronLeft } from "~/lib/icons/ChevronLeft";
-import { cn } from "~/lib/utils";
+import { capitalizeFirst, cn } from "~/lib/utils";
 import Row from "./layout/Row";
 import { Button } from "./ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 import { H3 } from "./ui/typography";
 
 type HeaderProps = {
@@ -14,7 +16,14 @@ type HeaderProps = {
 export default function Header({ className, children, title }: HeaderProps) {
   return (
     <Row className={cn("align-end mb-4", className)} gap={16}>
-      <Button icon={ChevronLeft} onPress={router.back} variant={"outline"} />
+        <Tooltip>
+            <TooltipTrigger>
+                      <Button icon={ChevronLeft} onPress={router.back} variant={"outline"} />
+            </TooltipTrigger>
+            <TooltipContent>
+                {capitalizeFirst(t("common.goBack"))}
+            </TooltipContent>
+        </Tooltip>
       <H3>{title}</H3>
       {children}
     </Row>
