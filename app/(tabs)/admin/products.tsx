24ea29c68523e69f29872eb085a1238fc660a4f1
Aloys CHERRIER <aloyscherry@gmail.com>
2025-09-09 11:59:04 +0200

    Refactoring : Better products, aligned with basetable

--- app/(tabs)/admin/products.tsx
+++ app/(tabs)/admin/products.tsx
@@ -1,20 +1,23 @@
 import { Link } from "expo-router";
 import { t } from "i18next";
 import React, { useState } from "react";
-import {
-  ActivityIndicator,
-  useWindowDimensions,
-  View
-} from "react-native";
+import { ActivityIndicator, useWindowDimensions, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Cylinder } from "~/assets/images/icons/Cylinder";
 import { Forklift } from "~/assets/images/icons/Forklift";
+import { Plus } from "~/assets/images/icons/Plus";
 import { Snowflake } from "~/assets/images/icons/Snowflake";
 import RootView from "~/components/layout/RootView";
 import Row from "~/components/layout/Row";
 import { ProductCategoryTable } from "~/components/table/ProductCategoryTable";
 import { Button } from "~/components/ui/button";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
 import { Text } from "~/components/ui/text";
 import { H3, P } from "~/components/ui/typography";
 import { useInfiniteFetchQuery } from "~/lib/hooks/useInfiniteFetchQuery";
@@ -23,22 +26,22 @@ import { capitalizeFirst } from "~/lib/utils";
 type stockGlobalCategory = "PF" | "MP" | "EMB";
 
 export default function App() {
-    
-    const [stockGlobalCategory, setStockGlobalCategory] = useState<stockGlobalCategory>("PF");
-    const GENERAL_STOCK_CATEGORIES = [
+  const [stockGlobalCategory, setStockGlobalCategory] =
+    useState<stockGlobalCategory>("PF");
+  const GENERAL_STOCK_CATEGORIES = [
     { value: "PF", label: t("stocks.finishedProducts"), icon: Forklift },
     { value: "MP", label: t("stocks.rawMaterials"), icon: Snowflake },
     { value: "EMB", label: t("stocks.packaging"), icon: Cylinder },
   ] as const;
 
-  const { data, isLoading, isError,fetchNextPage } = useInfiniteFetchQuery(
-    "/productCategory/{stock_global_category}", 
+  const { data, isLoading, isError, fetchNextPage } = useInfiniteFetchQuery(
+    "/productCategory/{stock_global_category}",
     "get",
     {
-        path : { stock_global_category : stockGlobalCategory },
-        query : { limit : 10}
+      path: { stock_global_category: stockGlobalCategory },
+      query: { limit: 10 },
     }
-);
+  );
   const { width } = useWindowDimensions();
   const insets = useSafeAreaInsets();
 
@@ -57,13 +60,15 @@ export default function App() {
 
   return (
     <RootView>
-    {/* Products */}
+      {/* Products */}
       <View className="gap-y-10">
         <Row className="justify-between">
           <H3 className="">{capitalizeFirst(t("common.products"))}</H3>
           <Select
             defaultValue={GENERAL_STOCK_CATEGORIES[0]}
-            value={GENERAL_STOCK_CATEGORIES.find((option) => option.value == stockGlobalCategory)}
+            value={GENERAL_STOCK_CATEGORIES.find(
+              (option) => option.value == stockGlobalCategory
+            )}
             onValueChange={(option) =>
               setStockGlobalCategory(
                 option?.value as (typeof GENERAL_STOCK_CATEGORIES)[number]["value"]
@@ -73,7 +78,9 @@ export default function App() {
           >
             <SelectTrigger>
               <SelectValue
-                placeholder={capitalizeFirst(t("product_category.selectStockGeneralCategory"))}
+                placeholder={capitalizeFirst(
+                  t("product_category.selectStockGeneralCategory")
+                )}
               >
                 <View className="mr-2 flex flex-row items-center">
                   {(() => {
@@ -87,8 +94,9 @@ export default function App() {
                   })()}
                   <P className="capitalize">
                     {
-                      GENERAL_STOCK_CATEGORIES.find((option) => option.value == stockGlobalCategory)
-                        ?.label
+                      GENERAL_STOCK_CATEGORIES.find(
+                        (option) => option.value == stockGlobalCategory
+                      )?.label
                     }
                   </P>
                 </View>
@@ -120,20 +128,20 @@ export default function App() {
             fetchNextPage={fetchNextPage}
           />
         )}
-    </View>
-    <View>
-      <Link
-        href={{
-          pathname: "/admin/add_new_product",
-          params: { stockCategory: stockGlobalCategory },
-        }}
-        asChild>
-        <Button
-          className="flex-1">
-            {capitalizeFirst(t("product_category.add_new_product"))}
-        </Button>
-      </Link>
-    </View>
+      </View>
+      <View>
+        <Link
+          href={{
+            pathname: "/admin/add_new_product",
+            params: { stockCategory: stockGlobalCategory },
+          }}
+          asChild
+        >
+          <Button icon={Plus} className="flex-1">
+            {capitalizeFirst(t("product_category.new_product"))}
+          </Button>
+        </Link>
+      </View>
     </RootView>
   );
 }
