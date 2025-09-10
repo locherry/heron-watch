89eaa6637739261a18dc4a50f72e370541645321
Aloys CHERRIER <aloyscherry@gmail.com>
2025-09-11 00:14:17 +0200

    UI : add success alert

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -1,9 +1,9 @@
-import { Icon } from '~/components/ui/icon';
-import { Text, TextClassContext } from '~/components/ui/text';
-import { cn } from '~/lib/utils';
-import type { LucideIcon } from 'lucide-react-native';
-import * as React from 'react';
-import { View, type ViewProps } from 'react-native';
+import type { LucideIcon } from "lucide-react-native";
+import * as React from "react";
+import { View, type ViewProps } from "react-native";
+import { Icon } from "~/components/ui/icon";
+import { Text, TextClassContext } from "~/components/ui/text";
+import { cn } from "~/lib/utils";
 
 function Alert({
   className,
@@ -15,27 +15,35 @@ function Alert({
 }: ViewProps &
   React.RefAttributes<View> & {
     icon: LucideIcon;
-    variant?: 'default' | 'destructive';
+    variant?: "default" | "destructive" | "success";
     iconClassName?: string;
   }) {
   return (
     <TextClassContext.Provider
       value={cn(
-        'text-sm text-foreground',
-        variant === 'destructive' && 'text-destructive',
+        "text-sm text-foreground",
+        variant === "destructive" && "text-destructive",
+        variant === "success" && "text-green-600",
         className
-      )}>
+      )}
+    >
       <View
         role="alert"
         className={cn(
-          'bg-card border-border relative w-full rounded-lg border px-4 pb-2 pt-3.5',
+          "bg-card border-border relative w-full rounded-lg border px-4 pb-2 pt-3.5",
           className
         )}
-        {...props}>
+        {...props}
+      >
         <View className="absolute left-3.5 top-3">
           <Icon
             as={icon}
-            className={cn('size-4', variant === 'destructive' && 'text-destructive', iconClassName)}
+            className={cn(
+              "size-4",
+              variant === "destructive" && "text-destructive",
+              variant === "success" && "text-green-600",
+              iconClassName
+            )}
           />
         </View>
         {children}
@@ -50,7 +58,10 @@ function AlertTitle({
 }: React.ComponentProps<typeof Text> & React.RefAttributes<Text>) {
   return (
     <Text
-      className={cn('mb-1 ml-0.5 min-h-4 pl-6 font-medium leading-none tracking-tight', className)}
+      className={cn(
+        "mb-1 ml-0.5 min-h-4 pl-6 font-medium leading-none tracking-tight",
+        className
+      )}
       {...props}
     />
   );
@@ -64,8 +75,8 @@ function AlertDescription({
   return (
     <Text
       className={cn(
-        'text-muted-foreground ml-0.5 pb-1.5 pl-6 text-sm leading-relaxed',
-        textClass?.includes('text-destructive') && 'text-destructive/90',
+        "text-muted-foreground ml-0.5 pb-1.5 pl-6 text-sm leading-relaxed",
+        textClass?.includes("text-destructive") && "text-destructive/90",
         className
       )}
       {...props}
