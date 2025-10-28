2e45083d87a13330c0487e6ef4a652750ca0ce29
Aloys CHERRIER <aloyscherry@gmail.com>
2025-10-28 22:42:27 +0100

    Scripts : move from commonjs to ES module

--- scripts/updateTranslation.mjs
+++ scripts/updateTranslation.mjs
@@ -2,7 +2,7 @@ import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { devEnvConfig } from '../devEnvConfig.env.ts';
+import { devEnvConfig } from '../devEnvConfig.env';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
