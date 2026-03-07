35a6b4dea8a4e8f38a1ad3c8738af4df21167a56
Aloys CHERRIER <aloyscherry@gmail.com>
2026-03-07 22:26:22 +0100

    I18n : update

--- translations/i18n.ts
+++ translations/i18n.ts
@@ -1,29 +1,32 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
 // import Backend from 'i18next-http-backend'
 // import LanguageDetector from 'i18next-browser-languagedetector'
 
-
-import mainDE from './DE/main.json'
-import mainEN from './EN/main.json'
-import mainEU from './EU/main.json'
-import mainFR from './FR/main.json'
+import mainDE from "./DE/main.json";
+import mainEN from "./EN/main.json";
+import mainES from "./ES/main.json";
+import mainEU from "./EU/main.json";
+import mainFR from "./FR/main.json";
 
 export const languageRessources = {
   EN: {
-    main: mainEN
+    main: mainEN,
   },
   FR: {
-    main: mainFR
+    main: mainFR,
   },
   DE: {
-    main: mainDE
+    main: mainDE,
   },
   EU: {
-    main: mainEU
-  }
-}
+    main: mainEU,
+  },
+  ES: {
+    main: mainES,
+  },
+};
 
 i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
@@ -38,13 +41,11 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'EN',
+    fallbackLng: "EN",
     resources: languageRessources,
-    ns: ['main'],
-    defaultNS: 'main',
+    ns: ["main"],
+    defaultNS: "main",
     debug: false, // disables logging
-  })
+  });
 
-
-
-export default i18n
+export default i18n;
