716d20a899b1246c5c34e1f0361bb8cd349825fb
Aloys CHERRIER <aloyscherry@gmail.com>
2026-03-29 20:13:29 +0200

    UI : enhanced default not found page

--- app/(root)/+not-found.tsx
+++ app/(root)/+not-found.tsx
@@ -1,11 +1,28 @@
-import { Text, View } from "react-native";
- 
+import { Link, router } from "expo-router";
+import { View } from "react-native";
+import Row from "~/components/layout/Row";
+import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold">
-        Not found
-      </Text>
+      <Text className="text-xl font-bold">Not found</Text>
+      <Row gap={16}>
+        <Link asChild href={"/home"}>
+          <Button> Go home</Button>
+        </Link>
+        <Button
+          onPress={() => {
+            if (router.canGoBack()) {
+              router.back();
+            }
+          }}
+          disabled={!router.canGoBack()}
+        >
+          Go back
+        </Button>
+      </Row>
     </View>
   );
 }
