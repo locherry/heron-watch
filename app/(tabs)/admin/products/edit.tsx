1d8054b1c8b211d1d34a65d98b07f7bda39822c8
Aloys CHERRIER <aloyscherry@gmail.com>
2026-06-30 10:47:57 +0200

    FIX : Resolve TS lint error

---
+++ app/(tabs)/admin/products/edit.tsx
@@ -0,0 +1,10 @@
+import { Text } from "~/components/ui/text";
+
+export default function App() {
+  return (
+    <>
+      <Text>Product Edit Page</Text>;
+      <Text>This page is under construction.</Text>
+    </>
+  );
+}
