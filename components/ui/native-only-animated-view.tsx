02275bdffa694beebef2c6c2b1fcb9c1e3ba9a0d
Aloys CHERRIER <aloyscherry@gmail.com>
2026-07-08 09:37:33 +0200

    Fix : TS error

--- components/ui/native-only-animated-view.tsx
+++ components/ui/native-only-animated-view.tsx
@@ -1,5 +1,5 @@
-import { Platform, Pressable } from 'react-native';
-import Animated from 'react-native-reanimated';
+import { Platform, Pressable } from "react-native";
+import Animated from "react-native-reanimated";
 
 const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
@@ -13,16 +13,25 @@ const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
  * </NativeOnlyAnimatedView>
  */
 function NativeOnlyAnimatedView(
-  props: (React.ComponentProps<typeof Animated.View> & React.RefAttributes<typeof Animated.View> 
-    & { as?: "View" }) | (React.ComponentProps<typeof AnimatedPressable> & React.RefAttributes<typeof AnimatedPressable> & { as: "Pressable" })
+  props:
+    | (React.ComponentProps<typeof Animated.View> &
+        React.RefAttributes<typeof Animated.View> & { as?: "View" })
+    | (React.ComponentProps<typeof AnimatedPressable> &
+        React.RefAttributes<typeof AnimatedPressable> & { as: "Pressable" }),
 ) {
-  if (Platform.OS === 'web') {
+  const { as, ...rest } = props;
+
+  if (Platform.OS === "web") {
     return <>{props.children as React.ReactNode}</>;
   } else {
-    if (props.as === "Pressable"){
+    if (as === "Pressable") {
       return <AnimatedPressable {...props} />;
     }
-    return <Animated.View {...props} />;
+    return (
+      <Animated.View
+        {...(rest as React.ComponentProps<typeof Animated.View>)}
+      />
+    );
   }
 }
 
